604c3b4b681acfac8811e5107cfe717df7ce5393
lennonalmeida <lennonraribeiro@gmail.com>
2019-02-15 11:55:05 -0300

    Bugs 1-RISOTO and 2-error from site solved

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ config = configparser.ConfigParser()
 config.read_file(open('config.ini'))
 updater = Updater(token=config['DEFAULT']['token'])
 dispatcher = updater.dispatcher
-
+erro = '--------------------------------------------------'
 def start(bot, update):
     me = bot.get_me()
 
     msg = "Fala manx!\n"
     msg += "Aqui é o {0} e eu posso te dizer quais as brocas de hoje.\n".format(me.first_name)
     msg += "/almoco para o almoço e /jantar para a janta\n"
-    msg += "Código: https://github.com/lennonalmeida/ufparubot\n"
+    #msg += "Código: https://github.com/lennonalmeida/ufparubot\n"
 
 
     main_menu_keyboard = [[telegram.KeyboardButton('/almoco')],
@@ -34,16 +34,21 @@ def start(bot, update):
 def almoco(bot, update):
     df = pd.read_html('http://ru.ufpa.br/index.php?option=com_content&view=article&id=7')
     cardapio = df[4].fillna(value='SEM REFEIÇÃO')
-    lunch = pd.DataFrame(cardapio[1])
+    
 
+    for i in range(1, 6):
+        if erro in cardapio[1][i]:
+            cardapio[1][i] = 'SEM REFEIÇÃO'
+ 
+    lunch = pd.DataFrame(cardapio[1])
     me = bot.get_me()
 
     today = str(date.today()).split('-')[-1]
     msg = ''
+
     for i in range(1, 6):
         if re.split("[- ]", cardapio[0][i])[-2] == today:
             msg = cardapio[1][i]
-
     a,b = treat(msg)
 
     bot.send_message(chat_id=update.message.chat_id, text=a)
@@ -53,36 +58,37 @@ def almoco(bot, update):
 def jantar(bot, update):
     df = pd.read_html('http://ru.ufpa.br/index.php?option=com_content&view=article&id=7')
     cardapio = df[4].fillna(value='SEM REFEIÇÃO')
+        
+    for i in range(1, 6):
+        if erro in cardapio[2][i]:
+            cardapio[2][i] = 'SEM REFEIÇÃO'
+    
     dinner = pd.DataFrame(cardapio[2])
-
-    me = bot.get_me()
+    me = bot.get_me() 
 
     today = str(date.today()).split('-')[-1]
     msg = ''
     for i in range(1, 6):
         if re.split("[- ]", cardapio[0][i])[-2] == today:
             msg = cardapio[2][i]
-    
+       
     c,d = treat(msg)
     
     bot.send_message(chat_id=update.message.chat_id, text=c)
     bot.send_message(chat_id=update.message.chat_id, text=d)
 
 def treat(msg):
-    if 'ARROZ CARRETEIRO' in msg:
+    if 'ARROZ CARRETEIRO' in msg or 'RISOTO DE FRANGO' in msg:        
         msg = msg.split('FEIJÃO')[0]
     else:
         msg = msg.split('ARROZ')[0]
     
     if msg == 'SEM REFEIÇÃO':
-        a, b = msg, ':('
+        a, b = msg, '😢'
     else:
         a = msg.split('VEGETARIANO:')[0].lower().capitalize()
         b = msg.split('VEGETARIANO:')[1].lower().strip().capitalize()
     return a, b
-
-
-
 
 
 db = redis.StrictRedis(host=config['DB']['host'],
